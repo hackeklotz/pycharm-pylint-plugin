5e41b04844465c4c00b290564afe45c5574416e9
hackeklotz <hackeklotz@mailbox.org>
2018-07-23 00:02:18 +0200

    Bug: Inspections warnings "module xyz not found"
    In the inspections were several warnings, that the module that should be parsed couldn't be found.
    Reason was that sometimes Pylint received relative paths as input, which of course couldn't be parsed.
    Underlying cause of this was that the given virtual file was a deprecated VirtualFileWindowsImpl.
    Hacky workaround now retrieves the original virtual file with the getDelegate() method.

--- src/main/java/com/github/hackeklotz/PylintInspector.java
+++ src/main/java/com/github/hackeklotz/PylintInspector.java
@@ -4,7 +4,8 @@ import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.injected.editor.VirtualFileWindowImpl;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,30 +16,36 @@ import java.util.List;
 
 public class PylintInspector extends LocalInspectionTool {
 
-	public static Logger logger = Logger.getInstance(PylintInspector.class);
+    @Override
+    public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager,
+                                         boolean isOnTheFly) {
+        VirtualFile vFile = file.getVirtualFile();
 
-	@Override
-	public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager,
-			boolean isOnTheFly) {
-		Path path = Paths.get(file.getVirtualFile().getCanonicalPath());
-		PylintExecutor executor = new PylintExecutor();
-		PropertiesComponent properties = PropertiesComponent.getInstance();
-		Path pylintPath = Paths.get(properties.getValue("pylint.path"));
-		String commandLineParameters = properties.getValue("pylint.commandLineParameters");
-		List<PylintError> errors = executor.execute(path, pylintPath, commandLineParameters);
-		List<ProblemDescriptor> problemDescriptors = transformToProblemDescriptions(errors,
-				file, manager);
-		return problemDescriptors.toArray(new ProblemDescriptor[0]);
-	}
+        // Workaround for the fact, that sometimes the virtual file is a strange deprecated VirtualFileWindowImpl
+        // Following thread seems correlated: https://intellij-support.jetbrains.com/hc/en-us/community/posts/360000407770-How-to-get-the-absolute-path-of-a-VirtualFile-
+        if (file.getVirtualFile() instanceof VirtualFileWindowImpl) {
+            vFile = ((VirtualFileWindowImpl) vFile).getDelegate();
+        }
+        Path path = Paths.get(vFile.getCanonicalPath());
+        PropertiesComponent properties = PropertiesComponent.getInstance();
+        Path pylintPath = Paths.get(properties.getValue("pylint.path"));
+        String commandLineParameters = properties.getValue("pylint.commandLineParameters");
 
-	private List<ProblemDescriptor> transformToProblemDescriptions(List<PylintError> pylintErrors,
-			@NotNull PsiFile file, @NotNull InspectionManager manager) {
-		List<ProblemDescriptor> problemDescriptors = new ArrayList<>();
-		for (PylintError pylintError : pylintErrors) {
-			ProblemDescriptor problemDescriptor = pylintError.createProblemDesriptor(file, manager);
-			problemDescriptors.add(problemDescriptor);
-		}
-		return problemDescriptors;
-	}
+        PylintExecutor executor = new PylintExecutor();
+        List<PylintError> errors = executor.execute(path, pylintPath, commandLineParameters);
+        List<ProblemDescriptor> problemDescriptors = transformToProblemDescriptions(errors,
+                file, manager);
+        return problemDescriptors.toArray(new ProblemDescriptor[0]);
+    }
+
+    private List<ProblemDescriptor> transformToProblemDescriptions(List<PylintError> pylintErrors,
+                                                                   @NotNull PsiFile file, @NotNull InspectionManager manager) {
+        List<ProblemDescriptor> problemDescriptors = new ArrayList<>();
+        for (PylintError pylintError : pylintErrors) {
+            ProblemDescriptor problemDescriptor = pylintError.createProblemDesriptor(file, manager);
+            problemDescriptors.add(problemDescriptor);
+        }
+        return problemDescriptors;
+    }
 
 }
